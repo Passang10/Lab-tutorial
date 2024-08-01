b418e0ffa4da5934bc6d6f8eadf92d23f983e15b
KelzangT <tashikelzang266@gmail.com>
2024-08-01 16:46:01 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+function triangle_area($base, $height) {
+    return ($base * $height) / 2;
+}
+
+// Example usage:
+$base = 3;
+$height = 5;
+echo "The area of the triangle is: " . triangle_area($base, $height);
+?>
+
+
+
+
